d229c02f54b3294f61fe79861c6f08de1276f73f
sharmaaman4419 <sharmaaman4419@gmail.com>
2020-01-10 11:45:16 +0530

    Updates

---
+++ Playlist_Application/precache-manifest.b04712b36ef48c39ecf9130e94357113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0f6e92e24abdd7568323acb608365c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3766df5fd1ec64511e3a",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "db2565661f88ed8a16f4",
+    "url": "/static/js/2.f1bed74b.chunk.js"
+  },
+  {
+    "revision": "3766df5fd1ec64511e3a",
+    "url": "/static/js/main.96bbcb0d.chunk.js"
+  },
+  {
+    "revision": "09b93799adb185479969",
+    "url": "/static/js/runtime-main.37261f0c.js"
+  }
+]);
